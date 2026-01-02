260ea184e4d43fd2e02a504d73ddc71e800fa0a4
Hafiz Pratama <hafizpratama0809@gmail.com>
2026-01-02 22:32:17 +0700

    setup

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,12 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
-    }
-
-    public function testDB()
-    {
-        $db = \Config\Database::connect();
-        return $db->connect() ? 'Koneksi OK' : 'Gagal';
+        
+        return view('home');
     }
 }
