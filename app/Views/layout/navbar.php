ed70346e6b7dd9785ca7b761edd5d8324165d066
Hafiz Pratama <hafizpratama0809@gmail.com>
2026-01-03 10:39:17 +0700

    setup tailwind

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,11 @@
+<nav class="flex w-full">
+    <a href="<?= base_url('/') ?>">Lovely Moments</a>
+    <ul class="flex">
+        <li><a href="<?= base_url('/') ?>">Home</a></li>
+        <li><a href="<?= base_url('/services') ?>">Services</a></li>
+        <li><a href="<?= base_url('/gallery') ?>">Gallery</a></li>
+        <li><a href="<?= base_url('/gallery') ?>">Packages</a></li>
+        <li><a href="<?= base_url('/about') ?>">Contact</a></li>
+        <li><a href="<?= base_url('/contact') ?>"><Button>BOOK NOW</Button></a></li>
+    </ul>
+</nav>
