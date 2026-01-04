4184be106797f90c1b96a76a024ddeb6605ac8e9
Hafiz Pratama <hafizpratama0809@gmail.com>
2026-01-04 23:47:54 +0700

    setup tailwind & navbar

--- app/Views/home.php
+++ app/Views/home.php
@@ -5,16 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/style.css?v=' . time()) ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
 
-<body>
-
-    <div class="w-full max-w-[1400px] grid grid-cols-1 items-center justify-center">
+<body class="bg-[#C63D5C] w-full flex justify-center">
+    <div class="w-full max-w-[2100px] grid grid-cols-1 items-center justify-center">
         <?= $this->include('layout/navbar') ?>
 
 
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,11 +1,11 @@
-<nav class="flex w-full">
-    <a href="<?= base_url('/') ?>">Lovely Moments</a>
-    <ul class="flex">
-        <li><a href="<?= base_url('/') ?>">Home</a></li>
-        <li><a href="<?= base_url('/services') ?>">Services</a></li>
-        <li><a href="<?= base_url('/gallery') ?>">Gallery</a></li>
-        <li><a href="<?= base_url('/gallery') ?>">Packages</a></li>
-        <li><a href="<?= base_url('/about') ?>">Contact</a></li>
-        <li><a href="<?= base_url('/contact') ?>"><Button>BOOK NOW</Button></a></li>
+<nav class="flex w-full py-4 px-20 justify-between  bg-gradient-to-l from-[#ff6186] to-[#C63D5C] text-[#FFF5F7]">
+    <a href="<?= base_url('/') ?>"><h1 class="font-semibold text-[24px]">Lovely Moments</h1></a>
+    <ul class="flex gap-10 items-center font-medium">
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/') ?>">Home</a></li>
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/services') ?>">Services</a></li>
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/gallery') ?>">Gallery</a></li>
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/gallery') ?>">Packages</a></li>
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/about') ?>">Contact</a></li>
+        <li><a class="text-[#FFF5F7]" href="<?= base_url('/contact') ?>"><Button>BOOK NOW</Button></a></li>
     </ul>
 </nav>
